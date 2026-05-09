e93d453dccd7567241e66345bd51c0f7137f0a34
magzhan_zh <jumabajmagzhan@gmail.com>
2026-05-09 22:42:58 +0500

    Refactor Course class constructor and add methods

--- src/Model1/Course.java
+++ src/Model1/Course.java
@@ -3,15 +3,29 @@ package Model1;
 import java.util.*;
 
 public class Course {
-
     private String courseCode;
     private String courseName;
     private int credits;
-    private List<Teacher> instructors;
     private List<Course> prerequisites;
-    private List<Lesson> lessons;
 
-    public Course() {
+    public Course(String code, String name, int credits) {
+        this.courseCode = code;
+        this.courseName = name;
+        this.credits = credits;
+        this.prerequisites = new ArrayList<>();
     }
 
+    public void addPrerequisite(Course course) {
+        this.prerequisites.add(course);
+    }
+
+    public List<Course> getPrerequisites() {
+        return prerequisites;
+    }
+
+    public String getCourseName() { return courseName; }
+    public int getCredits() { return credits; }
+    
+    @Override
+    public String toString() { return courseName + " (" + courseCode + ")"; }
 }
