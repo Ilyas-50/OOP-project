375cf693315b58aa9d005d636fac4ffe95977b3f
magzhan_zh <jumabajmagzhan@gmail.com>
2026-05-09 22:42:12 +0500

    Add method to add marks to Transcript

--- src/Model1/Transcript.java
+++ src/Model1/Transcript.java
@@ -3,11 +3,23 @@ package Model1;
 import java.util.*;
 
 public class Transcript {
-
     private List<Mark> marks;
 
     public Transcript() {
         this.marks = new ArrayList<>();
     }
 
+    public void addMark(Mark mark) {
+        this.marks.add(mark);
+    }
+
+    @Override
+    public String toString() {
+        if (marks.isEmpty()) return "Transcript is empty.";
+        StringBuilder sb = new StringBuilder("--- Academic Transcript ---\n");
+        for (Mark m : marks) {
+            sb.append(m.toString()).append("\n");
+        }
+        return sb.toString();
+    }
 }
