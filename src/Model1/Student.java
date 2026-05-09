e0ba5cf9409b54bca184e0799efbf278b1208e6d
magzhan_zh <jumabajmagzhan@gmail.com>
2026-05-09 22:41:50 +0500

    Refactor Student class methods and improve course registration

--- src/Model1/Student.java
+++ src/Model1/Student.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Student extends User {
-
     private int yearOfStudy;
     private int totalCredits;
     private double gpa;
@@ -16,56 +15,40 @@ public class Student extends User {
     public Student() {
         super();
         this.enrolledCourses = new ArrayList<>();
+        this.transcript = new Transcript();
     }
 
     public void viewCourses() {
-        // TODO implement here
+        System.out.println("Enrolled Courses:");
+        for (Course c : enrolledCourses) {
+            System.out.println("- " + c.getCourseName());
+        }
     }
 
-//    public void registerToCourse(Course course) throws CreditLimitExceededException {
-//        int MAX_CREDITS = 21;
-//        if (this.totalCredits + course.getCredits() > MAX_CREDITS) {
-//            throw new CreditLimitExceededException(
-//                    "Cannot register for " + course.getCourseName() +
-//                            ". Credit limit exceeded! Current: " + totalCredits +
-//                            ", Course: " + course.getCredits()
-//            );
-//        }
-//        // Если лимит не превышен, добавляем курс
-//        this.enrolledCourses.add(course);
-//        this.totalCredits += course.getCredits();
-//        System.out.println("Successfully registered to " + course.getCourseName());
-//    }
+    public void registerToCourse(Course course) {
+        int MAX_CREDITS = 21;
+        if (this.totalCredits + course.getCredits() > MAX_CREDITS) {
+            System.out.println("Error: Credit limit exceeded for " + course.getCourseName());
+            return;
+        }
+        this.enrolledCourses.add(course);
+        this.totalCredits += course.getCredits();
+        System.out.println("Successfully registered to " + course.getCourseName());
+    }
 
     public void viewTranscript() {
-        // TODO implement here
+        System.out.println(transcript.toString());
     }
 
-    public int getYearOfStudy() {
-        return yearOfStudy;
-    }
+    public int getYearOfStudy() { return yearOfStudy; }
+    public void setYearOfStudy(int yearOfStudy) { this.yearOfStudy = yearOfStudy; }
 
-    public void setYearOfStudy(int yearOfStudy) {
-        this.yearOfStudy = yearOfStudy;
-    }
+    public double getGpa() { return gpa; }
+    public void setGpa(double gpa) { this.gpa = gpa; }
 
-    public double getGpa() {
-        return gpa;
-    }
+    public Faculty getFaculty() { return faculty; }
+    public void setFaculty(Faculty faculty) { this.faculty = faculty; }
 
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-
-    public Faculty getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(Faculty faculty) {
-        this.faculty = faculty;
-    }
-
-    public List<Course> getEnrolledCourses() {
-        return enrolledCourses;
-    }
+    public List<Course> getEnrolledCourses() { return enrolledCourses; }
+    public Transcript getTranscript() { return transcript; }
 }
